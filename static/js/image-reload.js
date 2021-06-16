cf94dcf2061e0f6c0f60db0efedbd6c8242c429b
radoondas <ondas.radovan@gmail.com>
2021-06-16 15:14:42 +0200

    Adds webcam page, menu and script. Improves header section.

---
+++ static/js/image-reload.js
@@ -0,0 +1,11 @@
+'use strict';
+
+window.onload = function() {
+    const image = document.getElementById("webcam");
+
+    function updateImage() {
+        image.src = image.src.split("?")[0] + "?" + new Date().getTime();
+    }
+
+    setInterval(updateImage, 10000);
+};
